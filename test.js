c9e1e07921f3ae5033b818a787aff5104d8e1989
Alvis Prieditis <alvisprieditis@Alviss-Mac-mini.local>
2024-12-23 15:56:13 -0500

    test 2

--- test.js
+++ test.js
@@ -1,18 +1,27 @@
-const request = require('supertest')
-const app = require('./index')
+const request = require('supertest');
+const app = require('./index');
+let server;
+
+beforeAll((done) => {
+  server = app.listen(3000, done);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
 
 describe('GET /', () => {
   it('responds with Hello World!', async () => {
-    const response = await request(app).get('/')
-    expect(response.status).toBe(200)
-    expect(response.text).toBe('Hello World!')
-  })
-})  
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World!');
+  });
+});
 
 describe('GET /api/products', () => {
   it('responds with Products', async () => {
-    const response = await request(app).get('/api/products')
-    expect(response.status).toBe(200)
-    expect(response.text).toBe('Products')
-  })
-})
+    const response = await request(app).get('/api/products');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Products');
+  });
+});
